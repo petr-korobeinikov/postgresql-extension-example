9c63c20b8c5d80887a95fdea5d6c361a87841b8d
Petr A. Korobeinikov <pkorobeinikov@gmail.com>
2015-01-03 09:07:14 +0300

    Fix extension name.

--- ext/hello_ext--1.0.sql
+++ ext/hello_ext--1.0.sql
@@ -1,5 +1,5 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION hello_world" to load this file. \quit
+\echo Use "CREATE EXTENSION hello_ext" to load this file. \quit
 
 CREATE FUNCTION hello_world()
     RETURNS text
